bb3a232fb322ab13cce6591546883d867bbfdeaf
Igor Boiko <igorboiko@gmail.com>
2023-03-17 08:14:55 +0200

    add search with btn

--- src/index.js
+++ src/index.js
@@ -2,22 +2,37 @@
 import './css/styles.css';
 // Імпортуємо бібліотеки
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import axios from 'axios';
 import { getData } from './js/api';
+let items = [];
 let page = 1;
 let per_page = 40;
 let searchSubmit = '';
 
 const searchForm = document.querySelector('#search-form');
 const imagesList = document.querySelector('.gallery');
+const loadMore = document.querySelector('.load-more');
+loadMore.style.display = 'none';
 
 const handleSubmit = e => {
   e.preventDefault();
   const { value } = e.target.elements.searchQuery;
+  if (searchSubmit === value || !value) {
+    return;
+  }
   searchSubmit = value;
+  page = 1;
+  imagesList.innerHTML = '';
+  getImages();
+};
+const handleLoadMore = e => {
+  page++;
   getImages();
 };
 searchForm.addEventListener('submit', handleSubmit);
+loadMore.addEventListener('click', handleLoadMore);
 
 const renderImages = () => {
   const newImagesList = items
@@ -31,7 +46,7 @@ const renderImages = () => {
         comments,
         downloads,
       }) => `<div class="photo-card">
-            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <a href="${largeImageURL}" class="link overlay"><img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
             <div class="info">
               <p class="info-item">
                 <b>Likes: ${likes}</b>
@@ -49,15 +64,33 @@ const renderImages = () => {
           </div>`
     )
     .join('');
-  imagesList.innerHTML = '';
+  if (!page) {
+    imagesList.innerHTML = '';
+  }
   imagesList.insertAdjacentHTML('beforeend', newImagesList);
-  // плавне прокручування сторінки після запиту і відтворення кожної наступної групи зображень
-  const { height: cardHeight } =
-    imagesList.firstElementChild.getBoundingClientRect();
-  window.scrollBy({
-    top: cardHeight * 2,
-    behavior: 'smooth',
+
+  // if (imagesList.children.length >= totalHits) {
+  //   loadMore.style.display = 'none';
+  //   Notiflix.Notify.failure(
+  //     "We're sorry, but you've reached the end of search results."
+  //   );
+  // } else {
+  //   // Інакше показуємо кнопку «Завантажити ще»
+  //   // loadMore.style.display = 'block';
+  // }
+
+  new SimpleLightbox('.photo-card a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
   });
+  // плавне прокручування сторінки після запиту і відтворення кожної наступної групи зображень
+  // const { height: cardHeight } =
+  //   imagesList.firstElementChild.getBoundingClientRect();
+  // window.scrollBy({
+  //   top: cardHeight * 2,
+  //   behavior: 'smooth',
+  // });
 };
 
 function getImages() {
@@ -70,7 +103,7 @@ function getImages() {
         return;
       }
       // загальна кількість зображень
-      totalImages = data.totalHits;
+      totalHits = data.totalHits;
       items = data.hits;
       // посилання на маленьке зображення для списку карток.
       webformatURL = data.hits.webformatURL;
@@ -86,7 +119,17 @@ function getImages() {
       comments = data.hits.comments;
       // кількість завантажень.
       downloads = data.hits.downloads;
+      loadMore.style.display = 'none';
       renderImages();
+      if (imagesList.children.length >= totalHits) {
+        loadMore.style.display = 'none';
+        Notiflix.Notify.failure(
+          "We're sorry, but you've reached the end of search results."
+        );
+      } else {
+        // Інакше показуємо кнопку «Завантажити ще»
+        loadMore.style.display = 'block';
+      }
     })
     .catch(error => {
       console.error('your error:', error);
